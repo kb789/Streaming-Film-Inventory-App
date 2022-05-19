4b677517af6fc413d84f30936d673bc5b9f8b1b3
Kristina Bucher <bucher.kristina@gmail.com>
2022-05-18 23:21:16 -0400

    SECOND  commit

--- server.js
+++ server.js
@@ -31,5 +31,5 @@ app.use((req, res, next) => {
   res.status(404).send("<h1>Page not found on the server</h1>");
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log("Server is listening on port 3000");
